4eca46b12336667499211c6ccf33cad9a7ab3942
mericorhay <108902339+mericorhay@users.noreply.github.com>
2024-10-07 14:44:32 +0300

    Update YORUNGE.ino

--- YORUNGE.ino
+++ YORUNGE.ino
@@ -1,4 +1,17 @@
+#include <TinyGPS++.h>
+#include <TinyGPSPlus.h>
+#include <SoftwareSerial.h>
 #include <Servo.h>
+
+// GPS için seri iletişim
+int RX = 3;
+int TX = 2;
+int BAUD = 9600;
+unsigned long oncezaman = 0;
+const unsigned long gecikme = 3000;
+
+SoftwareSerial gpsseri(RX, TX);  // RX 3 TX 2
+TinyGPSPlus gps;
 
 // Servo motor tanımlamaları
 Servo solkanat;
@@ -19,18 +32,19 @@ double turev = 0;          // Türev terimi
 
 // ROLL PID değişkenleri
 double roll = 0;                   // Mevcut roll açısı
-double hedefroll = 0;              // Hedef roll açısı simülasyonu için
+double hedefroll = 1;              // Hedef roll açısı
 double rollhata, rollsonhata = 0;  // Roll hatası ve önceki hata
 double rollturev = 0;              // Roll türevi
 double rollintegral = 0;           // Roll integral
 double rollkontrolciktisi = 0;     // Roll kontrol çıkışı
 
 // PITCH PID değişkenleri
-double hedefburun = 0;     // Hedef burun açısı (pitch)
+double hedefburun = 1.0;  // Hedef burun açısı (pitch)
 double burun = 0;          // Mevcut burun açısı (sensörden güncellenmeli)
 double hata, sonhata = 0;  // Hata ve önceki hata
 double kontrolciktisi;     // Pitch kontrol çıkışı
 
+int irtifa = 0;
 // Diğer değişkenler
 bool motor = false;  // Motor durumu
 bool gear = true;    // İniş takımları durumu
@@ -44,6 +58,12 @@ double durum[2] = { 0.0, 0.0 };  // [mevcut hız, kontrol girişi]
 int numzamanadimi = 10;          // Zaman adımı sayısı
 unsigned long prevmilis = 0;     // Önceki zaman kaydedici
 long interval = 3000;            // Veri bildirim aralığı
+//Mevcut Heading
+int hedefhdg = 0;
+int mevcuthdg = 0;
+int hatahdg = 0;
+
+
 
 void setup() {
   // Servo motor pinleri atanıyor
@@ -54,6 +74,8 @@ void setup() {
 
   ignitionservo.attach(13);     // Motor ateşleme servosu
   ignitionoffservo.attach(14);  // Ateşlemeyi kapatan servo
+  gpsseri.begin(BAUD);
+  
 
   Serial.begin(9600);  // Seri iletişim başlatılıyor
 }
@@ -64,16 +86,89 @@ void loop() {
   pitchset();       // Pitch kontrol fonksiyonu
   rollset();        // Roll kontrol fonksiyonu
   veribildirimi();  // Veri bildirim fonksiyonu
-  delay(70);
+  delay(20);        // 70 ms gecikme
+  getgps();         // GPS verisi alımı
+}
+
+void heading() {
+  mevcuthdg = hdgal();
+  hatahdg = hedefhdg - mevcuthdg;
+  if (hatahdg < 5 && hatahdg > -5) {
+    hedefroll = 0;
+  } else {
+    for (int i = 0; i < 7; i++) {
+      hedefroll = i;
+    }
+  }
 }
 
 
 
+int hdgal() {
+  return 90;
+}
 
+void takeoff() {
+  hedefas = 60;
+  hedefroll = 0;
+  if (irtifa < 100) {
+    gear = true;
+  } else if (irtifa > 100) {
+    gear = false;
+  }
+  if (mevcutas < 60) {
+    hedefas = 120;
+    delay(250);
+    // TIRMANIŞ FONKSYİONUNA GEÇİŞ
 
+    for (int i = 0; i < 8; i++) {
+      hedefburun = i;
+    }
+    if (mevcutas < 50 && mevcutas > 60) {
+      Serial.println("V1 Hızına Ulaşıldı Kalkış İptal Edilemez.");
+    } else if (mevcutas < 60 && mevcutas > 70) {
+      Serial.println("VR Hızına Ulaşıldı Burun Kaldırılıyor.");
+    }
+  }
+}
+void getgps() {
+  unsigned long simdikizaman = millis();
+  if (simdikizaman - oncezaman >= gecikme) {
+    while (gpsseri.available() > 0) {
+      gps.encode(gpsseri.read());
+    }
 
+    // GPS başlatma mesajını burada yazdır
+    Serial.println("GPS Başlatıldı.");
 
+    // Geçerli konum kontrolü
+    if (gps.location.isValid()) {
+      Serial.print("Enlem: ");
+      Serial.println(gps.location.lat(), 6);
+      Serial.print("Boylam: ");
+      Serial.println(gps.location.lng(), 6);
+    } else {
+      Serial.println("GPS konumu geçersiz.");
+    }
 
+    // Geçerli hız kontrolü
+    if (gps.speed.isValid()) {
+      Serial.print("Hız (km/saat): ");
+      Serial.println(gps.speed.kmph());
+    } else {
+      Serial.println("GPS hızı geçersiz.");
+    }
+
+    // Geçerli yükseklik kontrolü
+    if (gps.altitude.isValid()) {
+      Serial.print("Yükseklik: ");
+      Serial.println(gps.altitude.meters());
+    } else {
+      Serial.println("Yükseklik geçersiz.");
+    }
+    oncezaman = simdikizaman;
+  }
+}
 
 // Hava hızı ölçüm fonksiyonu
 double getairspeed() {
@@ -95,6 +190,7 @@ void MPC() {
   }
 }
 
+// Veri bildirim fonksiyonu
 void veribildirimi() {
   unsigned long currentmilis = millis();
   if (currentmilis - prevmilis >= interval) {
@@ -116,18 +212,19 @@ void veribildirimi() {
   }
 }
 
+// Veri alım fonksiyonu
 void verialimi() {
   if (Serial.available() > 0) {
     String gelenveri = Serial.readStringUntil('\n');  // Seri porttan gelen veriyi oku
     gelenveri.trim();
     if (gelenveri.startsWith("roll:")) {
-      hedefroll = gelenveri.substring(5).toDouble();  // Roll açısını güncelle
+      roll = gelenveri.substring(5).toDouble();  // Roll açısını güncelle
     }
     if (gelenveri.startsWith("reset:true")) {
       reset();
     }
     if (gelenveri.startsWith("burun:")) {
-      hedefburun = gelenveri.substring(6).toDouble();  // Burun açısını güncelle
+      burun = gelenveri.substring(6).toDouble();  // Burun açısını güncelle
     }
     if (gelenveri == "motor_on") {
       engineon();  // Motoru aç
@@ -142,13 +239,14 @@ void rollset() {
   rollhata = hedefroll - roll;  // Roll hatasını hesapla
 
   // Eğer roll değeri hedefroll'e ulaştıysa, PID kontrolünü durdur
-  if (abs(rollhata) <= 0.5) {  // 0.5 derecelik bir tolerans
-    // Servoları mevcut açıda tutarak kilitleme durumu
-    solkanat.write(90);  // Sol kanat servo açısını sabit tut
-    sagkanat.write(90);  // Sağ kanat servo açısını sabit tut
-    return;              // Rollset fonksiyonunu burada sonlandır
+  if (abs(rollhata) <= 4) {  // 2 derecelik bir tolerans
+    solkanat.write(90);      // Sol kanat servo açısını sabit tut
+    sagkanat.write(90);      // Sağ kanat servo açısını sabit tut
+    rollintegral = 0;        // Integral terimini sıfırla
+    return;                  // Rollset fonksiyonunu burada sonlandır
   }
 
+  // Integral terimini güncelle
   rollintegral += rollhata;
 
   // Integral teriminin sınırlandırılması
@@ -162,26 +260,27 @@ void rollset() {
 
   rollkontrolciktisi = (oransalkat * rollhata) + (integralkat * rollintegral) + (turevkat * rollturev);
 
-  int servoaciroll1 = constrain(90 + rollkontrolciktisi, 60, 120);  // Sol kanat servo açısını sınırla
-  int servoaciroll2 = constrain(90 - rollkontrolciktisi, 60, 120);  // Sağ kanat servo açısını sınırla
+  int servoaciroll1 = constrain(90 + rollkontrolciktisi, 60, 110);  // Sol kanat servo açısını sınırla
+  int servoaciroll2 = constrain(90 - rollkontrolciktisi, 60, 110);  // Sağ kanat servo açısını sınırla
 
   solkanat.write(servoaciroll1);  // Sol kanat servo açısını güncelle
   sagkanat.write(servoaciroll2);  // Sağ kanat servo açısını güncelle
-  rollsonhata = rollhata;         // Son hatayı güncelle
+
+  rollsonhata = rollhata;  // Son hatayı güncelle
 }
 
-// Pitch PID kontrolü ve servoları kilitleme
+// Pitch PID kontrolü
 void pitchset() {
-  hata = hedefburun - burun;  // Pitch hatasını hesapla
+  hata = hedefburun - burun;  // Hata hesapla
 
   // Eğer burun değeri hedefburun'a ulaştıysa, PID kontrolünü durdur
-  if (abs(hata) <= 0.5) {  // 0.5 derecelik bir tolerans
-    // Kuyruk servosunu mevcut açıda tutarak kilitleme durumu
+  if (abs(hata) <= 2) {   // 0.5 derecelik bir tolerans
     kuyrukservo.write(90);  // Kuyruk servo açısını sabit tut
+    integral = 0;           // Integral terimini sıfırla
     return;                 // Pitchset fonksiyonunu burada sonlandır
   }
 
-  integral += hata;
+  integral += hata;  // Integral hesapla
 
   // Integral teriminin sınırlandırılması
   if (integral > max_integral) {
@@ -191,42 +290,39 @@ void pitchset() {
   }
 
   turev = hata - sonhata;  // Türev hesapla
+
   kontrolciktisi = (oransalkat * hata) + (integralkat * integral) + (turevkat * turev);
 
-  int servoacipitch = constrain(90 + kontrolciktisi, 60, 120);  // Kuyruk servo açısını sınırla
-  kuyrukservo.write(servoacipitch);                             // Kuyruk servo açısını güncelle
-  sonhata = hata;                                               // Son hatayı güncelle
+  int servoaci = constrain(90 + kontrolciktisi, 60, 120);  // Servo açısını sınırla
+
+  kuyrukservo.write(servoaci);  // Kuyruk servosu açısını güncelle
+
+  sonhata = hata;  // Son hatayı güncelle
 }
 
-// Motoru açan fonksiyon
+// Motor açma fonksiyonu
 void engineon() {
-  motor = true;               // Motor durumunu aç
-  ignitionservo.write(180);   // Ateşleme servosunu aç
-  ignitionoffservo.write(0);  // Motoru aç
-  Serial.println("Motor açıldı");
+  motorgazservosu.write(120);  // Gaz servosunu aç
+  ignitionservo.write(120);    // Ateşleme servosunu aç
+  motor = true;                // Motor durumunu aktif yap
+  delay(3000);
 }
 
-// Motoru kapatan fonksiyon
+// Motor kapama fonksiyonu
 void engineoff() {
-  motor = false;               // Motor durumunu kapat
-  ignitionoffservo.write(90);  // Motoru kapat
-  ignitionservo.write(0);      // Ateşleme servosunu kapat
-  Serial.println("Motor kapatıldı");
+  motorgazservosu.write(60);    // Gaz servosunu kapat  //!!!!!!!!!!!!!!!!!!!!!!!
+  ignitionoffservo.write(120);  // Ateşleme kapatma servosunu çalıştır //!!!!!!!!!!!!!!!!!!!!!!
+  motor = false;                // Motor durumunu pasif yap
+  delay(3000);
 }
 
+// Reset fonksiyonu
 void reset() {
   hedefroll = 0;
-  burun = 0;
-  roll = 0;
   hedefburun = 0;
-  rollkontrolciktisi = 0;
-  kontrolciktisi = 0;
-  rollhata = 0;
-  hata = 0;
-  rollturev = 0;
-  turev = 0;
-
-  Serial.println("Resetleme İşlemi Yapıldı LOOP geri dönüyor;");
-  delay(2000);
-  loop();
+  rollintegral = 0;
+  rollsonhata = 0;
+  integral = 0;
+  sonhata = 0;
+  Serial.println("RESET Gerçekleşti");
 }
